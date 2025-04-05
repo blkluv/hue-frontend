65ef98c9c6a9f24413004b685364bf7f42692203
The Wizard of Hahz <info@luvnft.com>
2025-04-05 08:34:00 -0400

    up

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
-import { getFullnodeUrl } from "@mysten/sui.js";
+import { getFullnodeUrl } from "@mysten/sui/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
